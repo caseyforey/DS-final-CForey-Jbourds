2c23dbff4e8270cb9eaead4afac1dc600ecf0f7f
Casey <cforey@uvm.edu>
2024-04-07 15:10:00 -0400

    Added reading set data

--- src/load/load_set_data.py
+++ src/load/load_set_data.py
@@ -8,6 +8,7 @@ Date Created: 4/7/2024
 """
 
 import pandas as pd
+import json
 
 def get_first_printings() -> pd.DataFrame:
     """
@@ -20,13 +21,21 @@ def get_first_printings() -> pd.DataFrame:
     pass
     
 
-def get_set_data() -> pd.DataFrame:
+def get_set_data(path) -> pd.DataFrame:
     """
     Function to get data about all of the sets into a Pandas DataFrame.
 
-    :param ???:
+    :param path: The path to the set json file to be read
 
-    :returns: Pandas DataFrame with every set and metadata about it.
+    :returns: Pandas DataFrame with every set and year it was released.
     """
-    pass
+    f = open(path, encoding= "utf8") 
+    set_list = json.load(f)
+    set_year = {}
+
+    for set_id in set_list['data']:
+        set_year[set_id['code']] = set_id['releaseDate'][0:4]
+    set_year_df = pd.DataFrame.from_dict(set_year, orient='index')
+    set_year_df.reset_index(inplace= True) 
+    set_year_df.rename(columns={'index': 'Set Name', 0:'Release Year'},inplace= True) 
 
